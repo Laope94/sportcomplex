a9bf7d5f10b665b9381b8cd1c0be0f1910312523
Laope94 <timotej.sulka@gmail.com>
2017-05-14 17:51:27 +0200

    bootstrap

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -13,6 +13,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	<meta charset="utf-8">
 	<title>Welcome to CodeIgniter</title>
+<style>
+    body{
+        padding: 5%;
+    }
+</style>
 
 </head>
 <body>
